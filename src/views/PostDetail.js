af4e4da73e852f889e2202341cab23a7bcc39ec6
andriyandriyan <andriy.andriyan1@gmail.com>
2018-10-01 02:30:17 +0700

    Add loading and remove footer

--- src/views/PostDetail.js
+++ src/views/PostDetail.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import moment from 'moment';
 import { http } from '../services';
+import loader from '../assets/images/loader.svg';
+import config from '../config';
 
 export default class PostDetail extends Component {
 
 	state = {
 		title: '',
-		content: ''
+		content: '',
+		loading: false
 	};
 
 	componentDidMount() {
+		window.scrollTo(0, 0);
 		const slug = this.props.match.params.slug;
+		this.setState({ loading: true });
 		http.get('posts?slug=' + slug).then(post => {
+			document.title = post[0].title.rendered + ' - ' + config.SITE_NAME;
 			this.setState({
 				title: post[0].title.rendered,
-				content: post[0].content.rendered
+				posted: post[0].date,
+				content: post[0].content.rendered,
+				loading: false
 			});
 		});
 	}
@@ -23,40 +31,52 @@ export default class PostDetail extends Component {
 		return { __html: html };
 	}
 
-	render() {
-		const { title, content } = this.state;
+	renderLoading() {
+		const windowHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+		const style = {
+			display: 'flex',
+			height: windowHeight,
+			justifyContent: 'center'
+		};
 		return (
-			<React.Fragment>
-				<header id="masthead" className="site-header" role="banner">
-					<div className="title-block">
-						<div className="entry-meta">
-							<span className="posted-on">Posted on <a><time className="entry-date published">April 1, 2018</time><time className="updated">September 26, 2018</time></a></span>
-							<span className="byline"> by <span className="author vcard"><a className="url fn n">andriyandriyan</a></span></span>
-						</div>
-						<h1>{title}</h1>
-					</div>
-				</header>
-				<div id="primary" className="content-area">
-					<div id="content" className="site-content" role="main">
-						<article id="post-190" className="post-190 post type-post status-publish format-standard hentry category-ionic tag-firebase tag-ionic tag-login tag-register libretto-long-form">
-							<div className="entry-content"
-								dangerouslySetInnerHTML={{__html: content}} />
-							<footer className="entry-meta">
-								<span className="cat-links">Posted in <a href="https://andriyandriyan.com/category/ionic" rel="category tag">Ionic</a></span><span className="tags-links">Tagged <a href="https://andriyandriyan.com/tag/firebase" rel="tag">Firebase</a>, <a href="https://andriyandriyan.com/tag/ionic" rel="tag">Ionic</a>, <a href="https://andriyandriyan.com/tag/login" rel="tag">Login</a>, <a href="https://andriyandriyan.com/tag/register" rel="tag">Register</a></span>
-							</footer>
-						</article>
-						<nav role="navigation" id="nav-below" className="navigation-post">
-							<h1 className="screen-reader-text">Post navigation</h1>
-							<div className="previous">
-								<a rel="prev"><span className="meta-nav">Previous Article</span> Membuat Grafik Di PHP-MySQL Menggunakan Chartjs</a>
-							</div>
-							<div className="next">
-							</div>
-						</nav>
-					</div>
-				</div>
-			</React.Fragment>
+			<div style={style}>
+				<img src={loader} alt="loader" />
+			</div>
 		);
+	}
+
+	render() {
+		const { title, content, loading } = this.state;
+
+		if (loading) {
+			return this.renderLoading();
+		}
+
+		if (!loading) {
+			const posted = moment(this.state.posted).format('D MMM YYYY');
+			return (
+				<React.Fragment>
+					<header id="masthead" className="site-header" role="banner">
+						<div className="title-block">
+							<div className="entry-meta">
+								<span className="posted-on">Posted on <a><time className="entry-date published">{posted}</time><time className="updated">September 26, 2018</time></a></span>
+								<span className="byline"> by <span className="author vcard"><a className="url fn n">andriyandriyan</a></span></span>
+							</div>
+							<h1>{title}</h1>
+						</div>
+					</header>
+					<div id="primary" className="content-area">
+						<div id="content" className="site-content" role="main">
+							<article id="post-190" className="post-190 post type-post status-publish format-standard hentry category-ionic tag-firebase tag-ionic tag-login tag-register libretto-long-form">
+								<div className="entry-content"
+									dangerouslySetInnerHTML={{__html: content}} />
+							</article>
+						</div>
+					</div>
+				</React.Fragment>
+			);
+		}
 	}
 
 }
